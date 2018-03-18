da2ff0c4d7a360217bd43f6fc634c5448f943fa2
Breno Wruck Schneider <brenows@gmail.com>
2018-03-18 07:33:29 -0300

    Add Product Edit with basic tabs

--- app/app.js
+++ app/app.js
@@ -17,11 +17,6 @@
           templateUrl: "app/products/productListView.html",
           controller: "ProductListCtrl as vm"
         })
-        .state("productEdit", {
-          url: "/products/edit/:productId",
-          templateUrl: "app/products/productEditView.html",
-          controller: "ProductEditCtrl as vm"
-        })
         .state("productDetail", {
           url: "/products/:productId",
           templateUrl: "app/products/productDetailView.html",
@@ -37,6 +32,35 @@
             }
           }
         })
+        .state("productEdit", {
+          abstract: true,
+          url: "/products/edit/:productId",
+          templateUrl: "app/products/productEditView.html",
+          controller: "ProductEditCtrl as vm",
+          resolve: {
+            productResource: "productResource",
+
+            product: function (productResource, $stateParams) {
+              var productId = $stateParams.productId;
+              return productResource.get({
+                productId: productId
+              }).$promise;
+            }
+          }
+        })
+        .state("productEdit.info", {
+          url: "/info",
+          templateUrl: "app/products/productEditInfoView.html",
+        })
+        .state("productEdit.price", {
+          url: "/price",
+          templateUrl: "app/products/productEditPriceView.html",
+        })
+        .state("productEdit.tags", {
+          url: "/tags",
+          templateUrl: "app/products/productEditTagsView.html",
+        })
+
     }
   ])
 
